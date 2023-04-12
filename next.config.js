9417f28223267138afc03817aa04a8b9909fc3d1
ray-1337 <33544674+ray-1337@users.noreply.github.com>
2023-04-12 02:00:06 +0200

    apply CSP to the app

--- next.config.js
+++ next.config.js
@@ -1,6 +1,27 @@
+const {nextSafe} = require("next-safe");
+
 /** @type {import('next').NextConfig} */
+
 const nextConfig = {
   reactStrictMode: true,
+
+  async headers () {
+		return [
+			{
+				source: '/:path*',
+				headers: nextSafe({
+          isDev: process.env.npm_lifecycle_event === "dev",
+          contentSecurityPolicy: {
+            mergeDefaultDirectives: true,
+            "default-src": ["'self'"],
+            "font-src": ["https://fonts.gstatic.com/"],
+            "style-src": ["'self'", "https://fonts.googleapis.com/css2"],
+            "script-src": ["'self'"]
+          }
+        }),
+			},
+		]
+	},
 }
 
-module.exports = nextConfig
+module.exports = nextConfig;
