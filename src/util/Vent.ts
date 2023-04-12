3a799eedafce83bf14cf95260d17308eb97b5c79
ray-1337 <33544674+ray-1337@users.noreply.github.com>
2023-04-12 13:31:38 +0200

    add deleteVent func [finale]

--- src/util/Vent.ts
+++ src/util/Vent.ts
@@ -1,15 +1,29 @@
 import {QuickDB} from "quick.db";
 const db = new QuickDB();
+const ventsID = "vents";
 
 export interface ventContent {
   message: string;
   date: Date;
+  ventID?: string;
 };
 
 export async function setVent(message: string) {
-  return await db.push<ventContent>("vents", {message, date: new Date()});
+  return await db.push<ventContent>(ventsID, { message, date: new Date(), ventID: Date.now().toString() });
 };
 
 export async function getVents() {
-  return await db.get<ventContent[]>("vents");
+  return await db.get<ventContent[]>(ventsID);
+};
+
+export async function deleteVent(ventID: string) {
+  try {
+    const vents = await getVents();
+    if (!vents?.length) return null;
+
+    return await db.set<ventContent[]>(ventsID, vents.filter(val => val.ventID !== ventID));
+  } catch (error) {
+    console.error(error);
+    return null;
+  };
 };
